9bb4cad75edb572b77fb49a262a0b6391a900644
Grupa 2 Java <seinho_>
2018-12-11 20:33:47 +0200

    Initial commit

---
+++ src/gui/DrawPanel.java
@@ -0,0 +1,21 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
+
+
+        g.setColor(new Color(red, green, blue));
+        g.fillOval(100,100, 30,30);
+
+      //  Image image = new ImageIcon ("img.jpg").getImage();
+  //      g.drawImage(image, 0, 0, this);
+
+    }
+}
